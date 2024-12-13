1ac292edb665d39c86f869ac0bc86c32fd566af0
heoheoyun <105352128+heoheoyun@users.noreply.github.com>
2024-12-14 01:06:20 +0900

    Update and rename Best.java to OC_01_02.java

--- ReportPackages/src/Report1/OC_01_02.java
+++ ReportPackages/src/Report1/OC_01_02.java
@@ -1,6 +1,6 @@
 package Report1;
 
-public class Best {
+public class OC_01_02 {
 	public static void main(String[] args) {
 		System.out.println("학기가 시작되었어요!");
 		System.out.println("이번 학기 자바를 열심히 공부해 최고의 Software 개발자가 될 거에요~~");
