ff3e074a7419c439f9f0eee707e24c03bfc43077
heoheoyun <105352128+heoheoyun@users.noreply.github.com>
2024-12-14 01:05:56 +0900

    Update and rename Welcome.java to OC_01_01.java

--- ReportPackages/src/Report1/OC_01_01.java
+++ ReportPackages/src/Report1/OC_01_01.java
@@ -1,6 +1,6 @@
 package Report1;
 
-public class Welcome {
+public class OC_01_01 {
 	public static void main(String[] args) {
 		System.out.println("자바는 처음이죠?");
 		System.out.println("Welcome to Java World~~");
